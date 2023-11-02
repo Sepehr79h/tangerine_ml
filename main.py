700ba0faf265f7bee148e4ee228d4010dc3734a9
Sepehr <sepehr79h@gmail.com>
2023-11-02 15:17:33 -0400

    updates
    Signed-off-by: Sepehr <sepehr79h@gmail.com>

--- main.py
+++ main.py
@@ -12,9 +12,13 @@
 # test_string_embedding = embeddings.embed_query(test_string)
 # breakpoint()
 # print(test_string_embedding)
+import torch
 from dataset import create_notebooks_data, get_llama_embeddings, create_dataset
 from models.SimpleNN import SimpleNN
 from train import train, test
+
+# Check for GPU availability and set the device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 notebooks_data = create_notebooks_data('notebooks.txt', 'id2stages.json')
 llama_embeddings = get_llama_embeddings()
@@ -22,6 +26,9 @@ train_X, train_y, test_X, test_y, val_X, val_y = create_dataset(notebooks_data, 
 input_dim = train_X.shape[1]
 hidden_dim = 128
 output_dim = train_y.shape[1]
-model = SimpleNN(input_dim, hidden_dim, output_dim)
-model, training_losses, validation_losses, validation_accuracies = train(model, train_X, train_y, val_X, val_y)
-test_accuracy = test(model, test_X, test_y)
+
+model = SimpleNN(input_dim, hidden_dim, output_dim).to(device)  # Move the model to the GPU if available
+
+model, training_losses, validation_losses, validation_accuracies = train(model, train_X, train_y, val_X, val_y, device)
+test_accuracy = test(model, test_X, test_y, device)
+
